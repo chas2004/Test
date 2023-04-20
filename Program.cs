bb540a035800741e54e0e7756454349d136782c7
chas2004 <chas2004@yandex.ru>
2023-04-20 15:50:08 -0500

    Commit message here

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿bool ContainsDuplicate(int[] nums){
+    HashSet<int> set = new HashSet<int>();
+    foreach(int num in nums){
+        if(set.Contains(num))
+            return true;
+        set.Add(num);
+    }
+    return false;
+
+}
+Console.WriteLine(ContainsDuplicate(new int[]{1,2,3,1}));
+Console.WriteLine(ContainsDuplicate(new int[]{1,2,3,4}));
